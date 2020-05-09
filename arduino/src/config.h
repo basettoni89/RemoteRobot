35cb444b1297a0ca7457345ccdd6f7e9c9422708
Davide Mortara <dmortara@alteanet.it>
2020-05-09 15:43:11 +0200

    Added RemoteDebug

--- arduino/src/config.h
+++ arduino/src/config.h
@@ -1,3 +1,5 @@
 #include "secrets.h"
 
+ #define LOGGING true
+
 #define HOSTNAME "robot-esp"
--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -3,9 +3,21 @@
 #include <ESP8266mDNS.h>
 #include <ArduinoOTA.h>
 #include "config.h"
+#include <RemoteDebug.h>
+
+#define DLOG(msg, ...) if(Debug.isActive(Debug.DEBUG)){Debug.printf(msg, ##__VA_ARGS__);}
+#define VLOG(msg, ...) if(Debug.isActive(Debug.VERBOSE)){Debug.printf(msg, ##__VA_ARGS__);}
+
+RemoteDebug Debug;
+
 
 void onOTAStart() {
   
+}
+
+void debugCallback() {
+    String cmd = Debug.getLastCommand();
+    DLOG("My command: %s\n", cmd.c_str());
 }
 
 void setup() {
@@ -22,8 +34,15 @@ void setup() {
   ArduinoOTA.setHostname((const char *)hostname.c_str());
   ArduinoOTA.begin();
   ArduinoOTA.onStart(onOTAStart);
+
+  Debug.begin((const char *)hostname.c_str());
+  Debug.setResetCmdEnabled(true);
+  Debug.setCallBackProjectCmds(debugCallback);
+  Debug.setSerialEnabled(false);
 }
 
 void loop() {
   ArduinoOTA.handle();
+  yield();
+  Debug.handle();
 }
